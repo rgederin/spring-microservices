b47f667dac924de00eb80fb2a9d70ae05ab65a07
rgederin <ruslan.gederin@goeuro.com>
2019-01-30 22:13:21 +0200

    dockerize eureka

--- licensing-service/src/main/docker/run.sh
+++ licensing-service/src/main/docker/run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 echo "********************************************************"
+echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
+echo "********************************************************"
+while ! `nc -z eurekaserver  $EUREKASERVER_PORT`; do sleep 3; done
+echo "******* Eureka Server has started"
+
+echo "********************************************************"
 echo "Waiting for the configuration server to start on port $CONFIGSERVER_PORT"
 echo "********************************************************"
 while ! `nc -z configserver $CONFIGSERVER_PORT `; do sleep 3; done
@@ -10,6 +16,9 @@ echo "********************************************************"
 
 
 echo "********************************************************"
-echo "Starting License Server with Configuration Service :  $CONFIGSERVER_URI";
+echo "Starting License Server with Configuration Service via Eureka :  $EUREKASERVER_URI" ON PORT: $SERVER_PORT;
 echo "********************************************************"
-java -Dspring.cloud.config.uri=$CONFIGSERVER_URI -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
+java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
+     -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
+     -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
+     -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
